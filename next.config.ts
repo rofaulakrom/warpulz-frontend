68c733085bd2044cbd99563735bbcc56aa6546ef
rofaulakrom <hendra141102@gmail.com>
2026-03-30 02:20:48 +0700

    Perbaiki tampilan

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! PERINGATAN BUKAN UNTUK PRODUKSI JANGKA PANJANG !!
-    // Ini mengabaikan error TypeScript agar bisa lolos Vercel Build
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Ini mengabaikan error ESLint agar bisa lolos Vercel Build
-    ignoreDuringBuilds: true,
   },
 };
 
